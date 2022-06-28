a214627d2d6ba97c3d1e94159c3435b1674a7b7c
arthur-cw <arthur.moreira@cloudwalk.io>
2022-06-28 20:58:53 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+### cloudrave
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use futures::StreamExt;
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, source::Source};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // Get a output stream handle to the default physical sound device
+    let (_stream, stream_handle) = OutputStream::try_default()?;
+
+    // let transport = web3::transports::Http::new("https://rpc.testnet.cloudwalk.io")?;
+    let transport = web3::transports::WebSocket::new("wss://mainnet.cloudwalk.io/ws").await?;
+    let web3 = web3::Web3::new(transport);
+
+    let brlc_contract = "0xA9a55a81a4C085EC0C31585Aed4cFB09D78dfD53";
+
+    let filter = web3::types::FilterBuilder::default()
+        .from_block(web3::types::BlockNumber::Latest)
+        .build();
+
+    // let mut logs = web3.eth_subscribe().subscribe_logs(filter).await?;
+    let mut logs = web3.eth_subscribe().subscribe_new_heads().await?;
+
+    while let Some(result) = logs.next().await {
+        let log = result?;
+        println!("{:?}", log);
+        // Load a sound from a file, using a path relative to Cargo.toml
+        let file = BufReader::new(File::open("assets/kick.ogg")?);
+        // Decode that sound file into a source
+        let source = Decoder::new(file)?;
+        // Play the sound directly on the device
+        stream_handle.play_raw(source.convert_samples());
+    }
+
+    Ok(())
 }
