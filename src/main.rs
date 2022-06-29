9b59254be7830b9643b10558be4a72dfabe8b24b
arthur-cw <arthur.moreira@cloudwalk.io>
2022-06-28 23:28:16 -0300

    feat: add 2 kicks per block; replaying each sound without reallocating new buffer

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,41 @@ use futures::StreamExt;
 use std::fs::File;
 use std::io::BufReader;
 use rodio::{Decoder, OutputStream, source::Source};
+use rodio;
+use std::io;
+use std::convert::AsRef;
+use std::sync::Arc;
+use std::io::Read;
+use std::time::Duration;
+
+pub struct Sound (Arc<Vec<u8>>);
+
+impl AsRef<[u8]> for Sound {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl Sound {
+    pub fn load(filename: &str) -> io::Result<Sound> {
+        let mut buf = Vec::new();
+        let mut file = File::open(filename)?;
+        file.read_to_end(&mut buf)?;
+        Ok(Sound(Arc::new(buf)))
+    }
+    pub fn cursor(self: &Self) -> io::Cursor<Sound> {
+        io::Cursor::new(Sound(self.0.clone()))
+    }
+    pub fn decoder(self: &Self) -> rodio::Decoder<io::Cursor<Sound>> {
+        rodio::Decoder::new(self.cursor()).unwrap()
+    }
+}
+
+// TODO restart if panic?
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    let sound = Sound::load("assets/kick.ogg")?;
     // Get a output stream handle to the default physical sound device
     let (_stream, stream_handle) = OutputStream::try_default()?;
 
@@ -24,12 +56,9 @@ async fn main() -> anyhow::Result<()> {
     while let Some(result) = logs.next().await {
         let log = result?;
         println!("{:?}", log);
-        // Load a sound from a file, using a path relative to Cargo.toml
-        let file = BufReader::new(File::open("assets/kick.ogg")?);
-        // Decode that sound file into a source
-        let source = Decoder::new(file)?;
-        // Play the sound directly on the device
-        stream_handle.play_raw(source.convert_samples());
+
+        stream_handle.play_raw(sound.decoder().convert_samples());
+        stream_handle.play_raw(sound.decoder().delay(Duration::from_millis(500)).convert_samples());
     }
 
     Ok(())
